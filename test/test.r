a9bc60d502e37e9b300a09c241d16e0580fc4c87
Andrea Galimberti <kronwiz@users.noreply.github.com>
2014-05-02 12:01:39 +0200

    Changed error handling.
    The error handling now uses "try" because the database object methods raise a user error.

--- test/test.r
+++ test/test.r
@@ -5,29 +5,22 @@ mysql: import %../mysqldb.so
 db: make mysql/database []
 
 print "^/* Connect: fail"
-res: db/connect "localhost" "test" "test" "foobar"
-
-print [ "res:" res type? res ]
-either res = false [
-	print [ "error:" db/error ]
+either error? res: try [ db/connect "localhost" "test" "test" "foobar" ] [
+	print res
 ] [
 	print [ "int-connection:" db/int-connection ]
 ]
 
 print "^/* Connect: success"
-res: db/connect "localhost" "test" "test" "test"
-
-print [ "res:" res type? res ]
-either res = false [
-	print [ "error:" db/error ]
+either error? res: try [ db/connect "localhost" "test" "test" "test" ] [
+	print res
 ] [
 	print [ "int-connection:" db/int-connection ]
 ]
 
 print "^/* Execute: select * from addressbook"
-res: db/execute "select * from addressbook"
-either res = false [
-	print [ "error:" db/error ]
+either error? res: try [ db/execute "select * from addressbook" ] [
+	print res
 ] [
 	print [ "int-result:" db/int-result ]
 	print [ "num-rows:" db/num-rows ]
@@ -47,9 +40,8 @@ db/set-autocommit false
 print [ "autocommit:" db/autocommit ]
 
 print "^/* Execute: select * from foobar -> fails"
-res: db/execute "select * from foobar"
-either res = false [
-	print [ "error:" db/error ]
+either error? res: try [ db/execute "select * from foobar" ] [
+	print res
 ] [
 	print [ "int-result:" db/int-result ]
 	print [ "num-rows:" db/num-rows ]
@@ -58,14 +50,16 @@ either res = false [
 
 
 print "^/* Close: success"
-db/close
-print [ "int-connection:" db/int-connection ]
-print [ "int-result:" db/int-result ]
+either error? res: try [ db/close ] [
+	print res
+] [
+	print [ "int-connection:" db/int-connection ]
+	print [ "int-result:" db/int-result ]
+]
 
 print "^/* Close: fail"
-res: db/close
-either res = false [
-	print [ "error:" db/error ]
+either error? res: try [ db/close ] [
+	print res
 ] [
 	print [ "int-connection:" db/int-connection ]
 ]
