0c7f62cb8afa499608f1055485754de767d4450a
Andrea Galimberti <kronwiz@users.noreply.github.com>
2014-05-06 23:19:19 +0200

    Added examples for insert and delete queries.

--- test/test.r
+++ test/test.r
@@ -35,10 +35,6 @@ while [ row <> none ] [
 	row: db/fetch-row
 ]
 
-print "^/* Set autocommit = false"
-db/set-autocommit false
-print [ "autocommit:" db/autocommit ]
-
 print "^/* Execute: select * from foobar -> fails"
 either error? res: try [ db/execute "select * from foobar" ] [
 	print res
@@ -48,6 +44,23 @@ either error? res: try [ db/execute "select * from foobar" ] [
 	print [ "num-cols:" db/num-cols ]
 ]
 
+print "^/* Execute: insert into addressbook"
+either error? res: try [ db/execute "insert into addressbook values( NULL, 'First', 'Last', '333.123.444', 'via di qua', 'non so', '9876', 'Italy', now() )" ] [
+	print res
+] [
+	print [ "Affected rows:" db/num-rows ]
+]
+
+print "^/* Execute: delete from addressbook"
+either error? res: try [ db/execute "delete from addressbook where userid = '11'" ] [
+	print res
+] [
+	print [ "Affected rows:" db/num-rows ]
+]
+
+print "^/* Set autocommit = false"
+db/set-autocommit false
+print [ "autocommit:" db/autocommit ]
 
 print "^/* Close: success"
 either error? res: try [ db/close ] [
